ba130e6b529796e8875c9aff91b16b6c681af1ce
Livelyoneweek <wlvvwhs@naver.com>
2023-03-02 22:12:32 +0900

    웹 서버와 서블릿 컨테이너 정리

--- src/main/java/hello/servlet/HelloServlet.java
+++ src/main/java/hello/servlet/HelloServlet.java
@@ -1,6 +1,5 @@
 package hello.servlet;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
